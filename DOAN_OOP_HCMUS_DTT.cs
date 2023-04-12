831b262352cbd0ddf61110956a273a015ace22c1
DcViet <111166640+DcViet@users.noreply.github.com>
2023-04-12 13:21:27 +0700

    Update DOAN_OOP_HCMUS_DTT.cs

--- DOAN_OOP_HCMUS_DTT.cs
+++ DOAN_OOP_HCMUS_DTT.cs
@@ -2,3 +2,45 @@
 MSSV: 218802**
 Đồ án Lập trình hướng đối tượng (OOP) K2-2022-2023 (12/2022 - 04/2023)
 */
+
+Do an LTHDT
+Đồ án LTHDT
+
+
+DEADLINE: 6:00 PM, 19/11/2022
+
+
+Viết phần mềm quản lý cửa hàng với các chức năng sau:
+
+Thêm, xóa, sửa và tìm kiếm các mặt hàng (mã, tên hàng, hạn dùng, công ty sản xuất, năm sản xuất, loại hàng).
+Thêm, xóa, sửa và tìm kiếm các loại hàng.
+Thêm, xóa, sửa và tìm kiếm các hóa đơn bán hàng
+Thêm, xóa, sửa và tìm kiếm các hóa đơn nhập hàng
+Thống kê số hàng còn lại trong kho theo từng mặt hàng
+Thống kê số hàng cũ đã hết hạn sử dụng
+
+Chú ý:
+
+Làm với giao diện web
+Các dữ liệu phải được lưu trữ trên tập tin
+Phải tổ chức theo mô hình 3 lớp.
+Không nộp chung mã nguồn với môn KTLT được.
+File doc tự đánh giá
+
+
+Hoàn thành
+
+Chưa hoàn thành
+
+Thêm, xóa, sửa và tìm kiếm các mặt hàng (mã, tên hàng, hạn dùng, công ty sản xuất, năm sản xuất, loại hàng)
+Ghi rõ tính năng hoàn thành
+
+
+...
+
+
+
+
+
+
+
