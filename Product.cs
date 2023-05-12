5c918eb5c8206aa56a62c72b35eba74b90d7f19e
DcViet <111166640+DcViet@users.noreply.github.com>
2023-05-12 15:29:44 +0700

    Create Product.cs

---
+++ Product.cs
@@ -0,0 +1,11 @@
+public class Product
+{
+    public int Code { get; set; }
+    public string Name { get; set; }
+    public DateTime ExpiryDate { get; set; }
+    public string ManufacturingCompany { get; set; }
+    public int YearOfManufacture { get; set; }
+    public string ProductType { get; set; }
+}
+
+
