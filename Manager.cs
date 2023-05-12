197b255c6c9fb308b86fe9501a0fb5219213577f
DcViet <111166640+DcViet@users.noreply.github.com>
2023-05-12 15:46:19 +0700

    Update Manager.cs

--- Manager.cs
+++ Manager.cs
@@ -185,3 +185,134 @@ public class StoreManager
     }
 }
 
+public class StoreManager
+{
+    private List<Product> products;
+    private List<RowType> rowTypes;
+    private List<SalesInvoice> salesInvoices;
+
+    public StoreManager()
+    {
+        products = new List<Product>();
+        rowTypes = new List<RowType>();
+        salesInvoices = new List<SalesInvoice>();
+    }
+
+    // Methods for managing products
+
+    public void AddProduct(Product product)
+    {
+        products.Add(product);
+    }
+
+    public void DeleteProduct(int code)
+    {
+        products.RemoveAll(p => p.Code == code);
+    }
+
+    public void EditProduct(int code, Product product)
+    {
+        int index = products.FindIndex(p => p.Code == code);
+        if (index != -1)
+        {
+            products[index] = product;
+        }
+    }
+
+    public List<Product> SearchProducts(Func<Product, bool> predicate)
+    {
+        return products.Where(predicate).ToList();
+    }
+
+    // Methods for managing row types
+
+    public void AddRowType(RowType rowType)
+    {
+        rowTypes.Add(rowType);
+    }
+
+    public void DeleteRowType(int id)
+    {
+        rowTypes.RemoveAll(r => r.Id == id);
+    }
+
+    public void EditRowType(int id, RowType rowType)
+    {
+        int index = rowTypes.FindIndex(r => r.Id == id);
+        if (index != -1)
+        {
+            rowTypes[index] = rowType;
+        }
+    }
+
+    public List<RowType> SearchRowTypes(Func<RowType, bool> predicate)
+    {
+        return rowTypes.Where(predicate).ToList();
+    }
+
+    // Methods for managing sales invoices
+
+    public void AddSalesInvoice(SalesInvoice salesInvoice)
+    {
+        salesInvoices.Add(salesInvoice);
+    }
+
+    public void DeleteSalesInvoice(int invoiceNumber)
+    {
+        salesInvoices.RemoveAll(si => si.InvoiceNumber == invoiceNumber);
+    }
+
+    public void EditSalesInvoice(int invoiceNumber, SalesInvoice salesInvoice)
+    {
+        int index = salesInvoices.FindIndex(si => si.InvoiceNumber == invoiceNumber);
+        if (index != -1)
+        {
+            salesInvoices[index] = salesInvoice;
+        }
+    }
+
+    public List<SalesInvoice> SearchSalesInvoices(Func<SalesInvoice, bool> predicate)
+    {
+        return salesInvoices.Where(predicate).ToList();
+    }
+
+    // Methods for saving and loading data to/from file
+
+    public void SaveToFile(string fileName)
+    {
+        using (StreamWriter writer = new StreamWriter(fileName))
+        {
+            foreach (Product product in products)
+            {
+                writer.WriteLine($"{product.Code},{product.Name},{product.ExpiryDate},{product.ManufacturingCompany},{product.YearOfManufacture},{product.ProductType}");
+            }
+        }
+        using (StreamWriter writer = new StreamWriter("rowtypes.txt"))
+        {
+            foreach (RowType rowType in rowTypes)
+            {
+                writer.WriteLine($"{rowType.Id},{rowType.Name}");
+            }
+        }
+        using (StreamWriter writer = new StreamWriter("salesinvoices.txt"))
+        {
+            foreach (SalesInvoice salesInvoice in salesInvoices)
+            {
+                writer.WriteLine($"{salesInvoice.InvoiceNumber},{salesInvoice.CustomerName},{salesInvoice.InvoiceDate}");
+                foreach (SalesInvoiceItem item in salesInvoice.Items)
+                {
+                    writer.WriteLine($"{item.ProductCode},{item.Quantity}");
+                }
+                writer.WriteLine("END");
+            }
+        }
+    }
+
+    public void LoadFromFile(string fileName)
+    {
+        using (StreamReader reader = new StreamReader(fileName))
+        {
+            while (!reader.EndOfStream)
+            {
+                string
+
