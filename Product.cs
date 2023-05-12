5fc30e9197fb136b05b66677a61213753ecde6fa
DcViet <111166640+DcViet@users.noreply.github.com>
2023-05-12 16:22:14 +0700

    Update Product.cs

--- Product.cs
+++ Product.cs
@@ -8,4 +8,21 @@ public class Product
     public string ProductType { get; set; }
 }
 
+public class RowType
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
 
+public Dictionary<string, int> GetRemainingStockByProduct()
+{
+    var remainingStock = products.GroupBy(p => p.Name)
+                                  .ToDictionary(g => g.Key, g => g.Sum(p => p.Quantity));
+    return remainingStock;
+}
+
+public int GetExpiredProductsCount()
+{
+    var expiredProductsCount = products.Count(p => p.ExpiryDate < DateTime.Now);
+    return expiredProductsCount;
+}
